a79bb9227b615768519e44248fbb31baf78aca1e
DCSCHUS <schuster@ucar.edu>
2017-03-20 11:39:42 -0600

    updated to support https

--- 5.0/content/tdm/runTdm.sh
+++ 5.0/content/tdm/runTdm.sh
@@ -1,1 +1,2 @@
-java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 2 -tds "http://rda-web-dev.ucar.edu:8080/" -cred xxx:xxx
+#java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 2 -tds "http://rda-web-dev.ucar.edu:8080/" -cred xxx:xxx
+java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 2 -tds "http://rda.ucar.edu:8443/" -cred
