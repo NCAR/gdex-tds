c08145ae78c16f28363c34d28d418c5f45dfcee6
Riley Conroy <rpconroy@ucar.edu>
2018-11-26 15:24:41 -0700

    Added TDM updates

--- 5.0/content/tdm/runTdm.sh
+++ 5.0/content/tdm/runTdm.sh
@@ -1,2 +1,5 @@
-#java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 2 -tds "http://rda-web-dev.ucar.edu:8080/" -cred xxx:xxx
-java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 2 -tds "http://rda.ucar.edu:8443/" -cred
+#java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 1 -tds "http://rda.ucar.edu:8080/" -cred tdm:tdsTrig
+
+#java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdm-5.0.jar -nthreads 1 -tds "https://rda.ucar.edu:8443/" -cred tdm:tdsTrig
+
+java -Xmx32g -Dtds.content.root.path=/usr/local/tds/content -jar tdmFat-5.0.0-beta5.jar -nthreads 1 -tds "https://rda.ucar.edu:8443/" -cred tdm:tdsTrig
