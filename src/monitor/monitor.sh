9f629e27b043a29fd8a0a3a51098ca4180503093
Riley Conroy <rpconroy@ucar.edu>
2019-02-04 10:51:33 -0700

    Added date to log

--- src/monitor/monitor.sh
+++ src/monitor/monitor.sh
@@ -4,7 +4,7 @@
 logsDir=/data/logs/tomcat
 log="$logsDir/numconnect"
 numOpenFiles=`/usr/sbin/lsof -u tomcat | wc | awk '{print $1}'`
-echo $numOpenFiles >> $log 2>&1
+echo "`date` -- $numOpenFiles" >> $log 2>&1
 
 ###################
 ### Error Check ###
@@ -20,10 +20,10 @@ if [[ $numOpenFiles -gt $openFileLimit ]]; then
     echo "$errorMsg" | mail -s "$subject" $mailRecipients
 fi
 
-#Check internal server errors 
+#Check internal server errors
 logDate=`date +%Y-%m-%d`
 linesInLastHour=300 # Estimate
-ISE=`tail -$linesInLastHour /data/logs/tomcat/localhost_access_log.${logDate}.txt | grep "500 "` 
+ISE=`tail -$linesInLastHour /data/logs/tomcat/localhost_access_log.${logDate}.txt | grep "500 "`
 if [[ $? -eq 0 ]]; then
 
     errorMsg="Internal server error in last hour:\n\n$ISE"
