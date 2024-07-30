78230175baa5950b56bc280cd2ee0bc50f0ceaf7
Riley Conroy <rpconroy@ucar.edu>
2024-07-30 10:12:56 -0600

    new catalogs

---
+++ src/change_dsid.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+import sys
+import os
+import re
+
+
+
+def main(filename, outfile):
+    ofile = open(outfile, 'w')
+    with open(filename) as fh:
+        for l in fh:
+            regex = r'ds([0-9][0-9][0-9])\.([0-9])'
+            regex_replacement = r'd\100\2'
+            str_output = re.sub(regex, regex_replacement, l)
+            ofile.write(str_output)
+            #print(l)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f'Convert dsxxx.x to dxxx.xxx')
+        print(f'usage: {sys.argv[0]} [filename] [outfile]')
+        exit(1)
+    main(sys.argv[1], sys.argv[2])
